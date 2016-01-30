8b55e3c2923f10f15e55ad7a1274fedeb62c814d
Lunik <guillaume.lunik@gmail.com>
2016-01-30 18:49:54 +0100

    add default callback

--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ const Settings = require('./settings.js')
 const Extractor = require('./extractor.js')
 
 function TVSTAPI () {
-
+  this.defaultCallback = function(data){console.log(data)}
 }
 
 TVSTAPI.prototype.getUser = function (callback) {
+  callback = typeof callback !== 'undefined' ? callback : this.defaultCallback
   Extractor.get({req: 'user'}, callback)
 }
 
 TVSTAPI.prototype.getToWatch = function (options, callback) {
   options = typeof options !== 'undefined' ? options : {}
   options.req = 'to_watch'
-  callback = typeof callback !== 'undefined' ? callback : function(data){console.log(data)}
+  callback = typeof callback !== 'undefined' ? callback : this.defaultCallback
 
   Extractor.get(options, callback)
 }
