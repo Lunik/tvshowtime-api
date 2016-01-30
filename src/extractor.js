7069d39d50ffbf87fa112573bf50d0ad9be797ae
Lunik <guillaume.lunik@gmail.com>
2016-01-30 18:41:35 +0100

    write data Extractor from api

---
+++ src/extractor.js
@@ -0,0 +1,17 @@
+const Settings = require('./settings.js')
+const Configurable = require('./configurable.js')
+
+var request = require('request')
+
+function Extractor () {
+}
+
+Extractor.prototype.get = function (options, callback) {
+  request(Configurable.set(options), function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(JSON.parse(body))
+    }
+  })
+}
+
+module.exports = new Extractor()
