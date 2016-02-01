0e956c7556354a5b864f3cc628c1117b7bc8e0e1
Lunik <guillaume.lunik@gmail.com>
2016-02-01 08:57:03 +0100

    add towatch proto and agenda prot

--- src/index.js
+++ src/index.js
@@ -7,5 +7,11 @@ TVSAPI.prototype.getUser = function(callback){
   API.get('user', {}, callback)
 }
 
+TVSAPI.prototype.getToWatch = function(options, callback){
+  API.get('to-watch', options, callback)
+}
 
+TVSAPI.prototype.getAgenda = function(options, callback){
+  API.get('agenda', options, callback)
+}
 module.exports = new TVSAPI()
