a862bc9dfc81ee69e4ea21936ef4a92a6d64ac9f
Lunik <guillaume.lunik@gmail.com>
2016-02-01 08:54:30 +0100

    add main api into main.js

---
+++ src/main.js
@@ -0,0 +1,34 @@
+const Settings = require('./settings.js')
+const Extractor = require('./extractor.js')
+
+function Main () {
+  this.defaultCallback = function (data) {console.log(data)}
+}
+
+Main.prototype.get = function (proto, options, callback) {
+  options = typeof options !== 'undefined' ? options : {}
+  callback = typeof callback !== 'undefined' ? callback : this.defaultCallback
+  //maybe verif que le proto existe
+  options.req = proto.toLowerCase()
+
+  Extractor.request(options, callback)
+}
+
+Main.prototype.set = function(proto, options, callback) {
+  options = typeof options !== 'undefined' ? options : {}
+  callback = typeof callback !== 'undefined' ? callback : this.defaultCallback
+  //maybe verif que le proto existe
+  options.req = proto.toLowerCase()
+
+  Extractor.request(options, callback)
+}
+
+Main.prototype.post = function(proto, options, callback){
+  options = typeof options !== 'undefined' ? options : {}
+  callback = typeof callback !== 'undefined' ? callback : this.defaultCallback
+  //maybe verif que le proto existe
+  options.req = proto.toLowerCase()
+
+    Extractor.post(options, callback)
+}
+module.exports = new Main()
