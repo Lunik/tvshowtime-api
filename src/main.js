98eb8a1916f5893144f2e633d9e304e85fe58b4f
Lunik <guillaume.lunik@gmail.com>
2016-02-01 09:17:49 +0100

    remove useless proto

--- src/main.js
+++ src/main.js
@@ -14,15 +14,6 @@ Main.prototype.get = function (proto, options, callback) {
   Extractor.request(options, callback)
 }
 
-Main.prototype.set = function(proto, options, callback) {
-  options = typeof options !== 'undefined' && options !== null ? options : {}
-  callback = typeof callback !== 'undefined' && callback !== null ? callback : this.defaultCallback
-  //maybe verif que le proto existe
-  options.req = proto.toLowerCase()
-
-  Extractor.request(options, callback)
-}
-
 Main.prototype.post = function(proto, options, callback){
   options = typeof options !== 'undefined' && options !== null ? options : {}
   callback = typeof callback !== 'undefined' && callback !== null ? callback : this.defaultCallback
