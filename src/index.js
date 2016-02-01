72fe6574da33ef21211750c5db5ed226c20d2736
Lunik <guillaume.lunik@gmail.com>
2016-02-01 08:19:33 +0100

    remove switch

--- src/index.js
+++ src/index.js
@@ -10,28 +10,8 @@ function TVSTAPI () {
 TVSTAPI.prototype.get = function (proto, options, callback) {
   options = typeof options !== 'undefined' ? options : {}
   callback = typeof callback !== 'undefined' ? callback : this.defaultCallback
-  switch (proto) {
-    case 'user':
-      options.req = 'user'
-      break
-    case 'to_watch':
-      options.req = 'to_watch'
-      break
-    case 'agenda':
-      options.req = 'agenda'
-      break
-    case 'library':
-      options.req = 'library'
-      break
-    case 'explore':
-      options.req = 'explore'
-      break
-    case 'show':
-      options.req = 'show'
-      break
-    default:
+  options.req = proto.toLowerCase()
 
-  }
   Extractor.get(options, callback)
 }
 
